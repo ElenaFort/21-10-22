b1e36eb027281785c0eaf70ef9aa618f8b12a4b9
Elena <fortygina.lena@mail.ru>
2022-11-11 11:28:13 +0500

    Zadacha 50

---
+++ Zadacha 50/Program.cs
@@ -0,0 +1,50 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента
+ в двумерном массиве, и возвращает значение этого элемента или же указание,
+что такого элемента нет. Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет*/
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int[,] InitMatrix(int m, int n)
+{
+    int[,] resultMatrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            resultMatrix[i, j] = rnd.Next(2, 10);
+        }
+    }
+    return resultMatrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = GetNumber("Введите число m:");
+int n = GetNumber("Введите число n:");
+int[,] matrix = InitMatrix(m, n);
+PrintMatrix(matrix);
+int m1 = GetNumber("Введите номер строки:");
+int n1 = GetNumber("Введите номер столбца:");
+
+if (m1 > matrix.GetLength(0) || n1 > matrix.GetLength(1))
+    Console.WriteLine("Такого числа в массиве нет");
+else
+    Console.WriteLine($"{matrix[m - 1, n - 1]}");
